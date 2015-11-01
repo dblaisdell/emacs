a939951ffbbae43903bc87fe2be0396af70c90cb
lunk djedi <lunk.djedi@gmail.com>
2015-11-01 16:10:59 -0500

    Adding clj refactor mode

--- init.el
+++ init.el
@@ -6,17 +6,18 @@
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
  '(custom-enabled-themes (quote (solarized-dark)))
- '(custom-theme-load-path  (quote ("~/.emacs.d/elpa/solarized-theme-20150916.504")))
  '(custom-safe-themes
-   (quote ("a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "13a67c5968379752e55fcb3960125f809ae6230c7711ecbd3aed4f1cc66cf71a" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+   (quote
+    ("a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "13a67c5968379752e55fcb3960125f809ae6230c7711ecbd3aed4f1cc66cf71a" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(custom-theme-load-path (quote ("~/.emacs.d/elpa/solarized-theme-20150916.504")))
  '(inhibit-startup-screen t)
- '(org-babel-load-languages (quote ((emacs-lisp . t) (plantuml . t))))
+ '(org-agenda-files (quote ("c:/Users/lunk/git/notes/clojure.org")))
+ '(org-babel-load-languages (quote ((emacs-lisp . t) (plantuml . t) (clojure . t))))
  '(org-confirm-babel-evaluate nil)
  '(org-plantuml-jar-path (expand-file-name "~/.emacs.d/plantuml.jar"))
  '(org-src-fontify-natively t)
- '(rainbow-delimiters-max-face-count 9)
- '(idle-highlight-idle-time 2.0)
- '(projectile-switch-project-action 'neotree-projectile-action))
+ '(projectile-switch-project-action (quote neotree-projectile-action))
+ '(rainbow-delimiters-max-face-count 9))
 
 ;;Setq variables
 (setq
@@ -48,6 +49,7 @@
 (require 'package)
 ;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/"))
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
 (package-initialize)
@@ -60,7 +62,7 @@
 			     htmlize magit exec-path-from-shell
 			     puppet-mode puppetfile-mode git git-blame
 			     neotree apache-mode align-cljlet mvn solarized-theme
-			     markdown-mode bundler inf-ruby))
+			     markdown-mode bundler inf-ruby cljsbuild-mode))
 
 (dolist (p my-packages)
   (unless (package-installed-p p)
@@ -105,7 +107,8 @@
 			       (paredit-mode)
 			       (rainbow-delimiters-mode)
 			       (idle-highlight-mode)
-			       (show-paren-mode)))
+			       (show-paren-mode)
+			       (clj-refactor-mode)))
 ;; Emacs Lisp
 (add-hook 'emacs-lisp-mode-hook (lambda ()
 				  (auto-complete-mode)
@@ -138,5 +141,5 @@
      "Window '%s' is normal")
    (current-buffer)))
 
-;; Press [pause] key in each window you want to "freeze"
+;; Press [f9] key in each window you want to "freeze"
 (global-set-key [f9] 'toggle-window-dedicated)
